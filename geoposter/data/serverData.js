3141f7fadcf040f30095e0fb34b18e9ad7ec14ec
Oscar Fonts <oscar.fonts@gmail.com>
2013-05-09 13:48:10 +0200

    Persist changes locally

---
+++ geoposter/data/serverData.js
@@ -0,0 +1,264 @@
+var serverData = {
+   "type":"FeatureCollection",
+   "features":[
+      {
+         "type":"Feature",
+         "properties":{
+            "Name":"Lloc de trobada",
+            "Description":"Avda Diagonal amb Plaça de les Glòries Catalanes, a les 9:30h.<br><br>Hàbitat Urbà.<br><br>"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               2.187241665992716,
+               41.40270486231625,
+               0
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "Name":"1 - Aiguabarreig Ripoll-Besòs",
+            "Description":"Imatges de la riuada de l'any 1962 a l'Aiguabarreig:<br><br><img src=\"http://www.riuadamontcada1962.com/picture/55828/\" alt=\"\" height=\"259\" width=\"373\"><br><br><img src=\"http://www.riuadamontcada1962.com/picture/55830/\" alt=\"\" height=\"257\" width=\"372\"><b><br> </b><br>"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               2.19111629867951,
+               41.48936223516744,
+               0
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "Name":"2 - Rec Comtal",
+            "Description":"Història del Rec Comtal:<br> <a href=\"http://el-rec-comtal.blogspot.com.es/\" rel=\"nofollow\" target=\"_blank\">http://el-rec-comtal.blogspot.com.es/</a><br><br>"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               2.186699196608572,
+               41.46611006983535,
+               0
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "Name":"3 - Barri Singuerlin",
+            "Description":"Poesia a Puig Castellar:<br><a href=\"http://www.youtube.com/watch?feature=player_embedded&amp;v=d-sbPhQFaPk\" rel=\"nofollow\" target=\"_blank\">http://www.youtube.com/watch?feature=player_embedded&amp;v=d-sbPhQFaPk</a><br><br>"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               2.198832262251298,
+               41.4629975909134,
+               0
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "Name":"4 - Parc Fluvial i Can Zam",
+            "Description":"<ul><li>Informació sobre el parc: <a href=\"http://www.amb.cat/web/mmamb/parcs/can_zam\" rel=\"nofollow\" target=\"_blank\">http://www.amb.cat/web/mmamb/parcs/can_zam</a> </li></ul><ul><li> Festivals de Radio Teletaxi aCan Zam:&nbsp;<a rel=\"nofollow\" target=\"_blank\">http://hemeroteca-paginas.lavanguardia.com/LVE01/PUB/2000/04/10/LVG20000410001VIB.pdf#navpanes=1&amp;sea...</a> <br></li></ul><ul><li> Planol guia de la feria de Abril de 1997 a Can Zam: <a href=\"http://hemeroteca-paginas.lavanguardia.com/LVE01/PUB/1997/04/25/SU219970425-013.pdf\" rel=\"nofollow\" target=\"_blank\">http://hemeroteca-paginas.lavanguardia.com/LVE01/PUB/1997/04/25/SU219970425-013.pdf</a></li></ul><ul><li>Notícia sobre la construcció de can Zam: <a href=\"http://hemeroteca-paginas.lavanguardia.com/LVE01/PUB/1996/06/12/LVG19960612-034.pdf#navpanes=1&amp;search=can\" rel=\"nofollow\" target=\"_blank\">http://hemeroteca-paginas.lavanguardia.com/LVE01/PUB/1996/06/12/LVG19960612-034.pdf#navpanes=1&amp;searc...</a><br></li></ul>"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               2.198887565320135,
+               41.45363927775991,
+               0
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "Name":"passejada al marge esquerra",
+            "Description":"LA PRESA INFLADA <BR> http://www.panoramio.com/photo_explorer#view=photo&position=2415&with_photo_id=9419051&order=date_desc&user=1024701"
+         },
+         "geometry":{
+            "type":"LineString",
+            "coordinates":[
+               [
+                  2.200805128360925,
+                  41.45110287465644,
+                  0
+               ],
+               [
+                  2.202240552472019,
+                  41.44971427791402,
+                  0
+               ],
+               [
+                  2.204372691937082,
+                  41.44766081192255,
+                  0
+               ],
+               [
+                  2.206997668440545,
+                  41.44455991066842,
+                  0
+               ]
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "Name":"5 - Barri del Fondo",
+            "Description":"<ul><li>Sobre la nova immigració: <a href=\"http://www.elperiodico.cat/ca/noticias/santa-coloma/fondo-santa-coloma-reconeixement-public-les-seves-llengues-2372498\" rel=\"nofollow\" target=\"_blank\">http://www.elperiodico.cat/ca/noticias/santa-coloma/fondo-santa-coloma-reconeixement-public-les-seve...</a></li></ul><ul><li>Una mirada dels anys 70:<br></li></ul><img src=\"https://lh3.googleusercontent.com/--m39sgq5MoU/ReqkemZusTI/AAAAAAAABOQ/nxNiI8bsIaQ/s729/Barri06.jpeg\" alt=\"\" height=\"271\" width=\"398\">"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               2.222815433839444,
+               41.4521010253276,
+               0
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "Name":"6 - Parc del Molinet",
+            "Description":"Informació sobre el Parc metropolità:  <br> <a href=\"http://www.amb.cat/c/document_library/get_file?p_l_id=5734&amp;folderId=1179536&amp;name=DLFE-13068.pdf\" rel=\"nofollow\" target=\"_blank\">http://www.amb.cat/c/document_library/get_file?p_l_id=5734&amp;folderId=1179536&amp;name=DLFE-13068.pdf</a><br><br>"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               2.211842685459376,
+               41.43775859339416,
+               0
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "Name":"7 - PereIV, Museu de la immigració",
+            "Description":"<ul><li>Exposició proposta Ajuntament de Barcelona sobre el carrer Pere IV: <a href=\"http://www.slideshare.net/Barcelona_cat/leix-pere-iv-eix-cvic-i-patrimonial-del-poblenou-de-la-ciutadella-al-bess\" rel=\"nofollow\" target=\"_blank\">http://www.slideshare.net/Barcelona_cat/leix-pere-iv-eix-cvic-i-patrimonial-del-poblenou-de-la-ciuta...</a></li></ul><ul><li>  Informació sobre el Museu de la Immigració: <a href=\"http://oliba.uoc.edu/mhic/index.php?option=com_frontpage&amp;Itemid=1\" rel=\"nofollow\" target=\"_blank\">http://oliba.uoc.edu/mhic/index.php?option=com_frontpage&amp;Itemid=1</a></li></ul><br>"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               2.212080338934676,
+               41.42800515064239,
+               0
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "Name":"8 - Barri de La Catalana - Marge dret del riu Besòs",
+            "Description":"<ul><li>Vídeo de l'any 2009 sobre la inaguració del Pont de la Catalana: <a href=\"http://www.tv3.cat/videos/1473129\" rel=\"nofollow\" target=\"_blank\">http://www.tv3.cat/videos/1473129</a></li></ul><ul><li>Vídeo promocional de l'operació immobiliària: <a href=\"http://vimeo.com/5231919\" rel=\"nofollow\" target=\"_blank\">http://vimeo.com/5231919</a></li></ul><ul><li>Web dels infografistes de la promotora: <a href=\"http://www.southvisions.com/proyectos/ver/arquitectura_2/leixample-blau_73_T/0/imagen\" rel=\"nofollow\" target=\"_blank\">http://www.southvisions.com/proyectos/ver/arquitectura_2/leixample-blau_73_T/0/imagen</a></li></ul><br>"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               2.219621731865867,
+               41.42587674680836,
+               0
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "Name":"passejada al merge dret",
+            "Description":""
+         },
+         "geometry":{
+            "type":"LineString",
+            "coordinates":[
+               [
+                  2.219434147627737,
+                  41.42624345691679,
+                  0
+               ],
+               [
+                  2.219739714638791,
+                  41.42564827540588,
+                  0
+               ],
+               [
+                  2.220413316382295,
+                  41.42530250969388,
+                  0
+               ],
+               [
+                  2.221340277480595,
+                  41.42468823579696,
+                  0
+               ],
+               [
+                  2.222226828022653,
+                  41.4243305570384,
+                  0
+               ],
+               [
+                  2.222808391626367,
+                  41.42396944589342,
+                  0
+               ],
+               [
+                  2.223721339252505,
+                  41.42357916542566,
+                  0
+               ]
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "Name":"9 - Les tres xemeneies",
+            "Description":"Dossier sobre la història de la central de producció energètica, redactat per la plataforma 3xemenies:<br><br> <a href=\"http://issuu.com/plataforma3xemeneies/docs/les_tres_xemeneies_de_sant_adri__v1.1?mode=window&amp;viewMode=singlePage\" rel=\"nofollow\" target=\"_blank\">http://issuu.com/plataforma3xemeneies/docs/les_tres_xemeneies_de_sant_adri__v1.1?mode=window&amp;viewMod...</a>"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               2.237465908959929,
+               41.42890581502682,
+               0
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "Name":"10 - Parc de la Pau",
+            "Description":"<ul><li>Elements de mobiliari urbà: <a href=\"http://www.escofet.es/pages/proyectos/ficha_proyectos.aspx?IdP=42\" rel=\"nofollow\" target=\"_blank\">http://www.escofet.es/pages/proyectos/ficha_proyectos.aspx?IdP=42</a></li></ul><ul><li>Vídeo casolà: <a href=\"http://vimeo.com/63033498\" rel=\"nofollow\" target=\"_blank\">http://vimeo.com/63033498</a></li></ul>"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               2.231205519846563,
+               41.41884784189073,
+               0
+            ]
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "Name":"Dinar al Port Fòrum",
+            "Description":"La web del restaurant: <a href=\"http://www.restauranteportnautic.com/\" rel=\"nofollow\" target=\"_blank\">http://www.restauranteportnautic.com/</a><br><br>"
+         },
+         "geometry":{
+            "type":"Point",
+            "coordinates":[
+               2.226682329778653,
+               41.41457994783396,
+               0
+            ]
+         }
+      }
+   ]
+}
